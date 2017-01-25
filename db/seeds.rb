e6625cbe212cd7361bcfe0d1c402f8e0af0fc312
Telephonist <chervonobab@gmail.xom>
2017-01-25 23:27:54 +0300

    fourth-task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://www.languagedaily.com/learn-german/vocabulary/common-german-words'
+html = open(url)
+
+doc = Nokogiri::HTML(html)
+
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+
+germans = doc.css('td.bigLetter')
+germans.each do |german|
+  puts "German is: #{german.text}"
+  puts "English is: #{german.next_element.text}"
+  Card.create(original_text: german.text, translated_text: german.next_element.text)
+end
